306c5d16de550c6208473ad01786c85bd560300f
Fabrício Zimmerer Murta <fabricio@object.net>
2019-10-04 16:02:13 -0300

    Updates constants to paths.
    - Added a constant to the Ext JS packages' 'packages' directory (where
      the sub-packages are located, like core, ux, soap, google)
    - Removed the full hardcoded path for the 'core' package
    - Updates path for jsduck binary from 6.0.0-beta to 7.0.0.pre.alpha.
    
    The github repository with the jsduck sources is available at:
        fabriciomurta/jsduck

--- generator/c2.tools.ExtTS/ExtTS_GUI/Constants.cs
+++ generator/c2.tools.ExtTS/ExtTS_GUI/Constants.cs
@@ -10,9 +10,9 @@ namespace ExtTS_GUI
     {
         public static readonly string ExtJS_Classic_Path = "classic\\classic\\src";
         public static readonly string ExtJS_Modern_Path = "modern\\modern\\src";
-        public static readonly string ExtJS_Core_Path = "packages\\core\\src";
+        public static readonly string ExtJS_Packages_Root = "packages";
 
-        public static readonly string JSDuckBin_Path = "0.tools\\jsduck-6.0.0-beta.exe";
+        public static readonly string JSDuckBin_Path = "0.tools\\jsduck-7.0.0.pre.alpha.exe";
         public static readonly string JSDuckOut_BasePath = "2.docs";
 
         public static readonly string ExtTSOut_BasePath = "3.out";
